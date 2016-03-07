1b0dead20361a7a90681e67db261d8e90f59882d
Keith Moore <keith.m4@gmail.com>
2016-03-07 15:42:15 -0500

    updates
    in class project

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 function showIcons() {
+
 	var iconsMenu = document.getElementById('app-icons');
 	
-	if (iconsMenu.style.display === 'none') {
+	if ((iconsMenu.style.display === 'none') || (iconsMenu.style.display === '')) {
 		iconsMenu.style.display = 'block';
 	} else {
 		iconsMenu.style.display = 'none';
