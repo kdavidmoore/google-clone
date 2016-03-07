f09b7643aa8a3d071fe7ae43f2b26e0beab6cd65
Keith Moore <keith.m4@gmail.com>
2016-03-07 15:03:19 -0500

    App icons menu
    Click the grid icon, and the app icons menu pops up. Have to click
    twice for some reason…

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function showIcons() {
+	var iconsMenu = document.getElementById('app-icons');
+	
+	if (iconsMenu.style.display === 'none') {
+		iconsMenu.style.display = 'block';
+	} else {
+		iconsMenu.style.display = 'none';
+	}
+}
